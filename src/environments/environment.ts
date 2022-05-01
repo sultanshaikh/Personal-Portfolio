4829e716e73fc3f65ab42b63b4f0ceb9f512dc32
Sultan Shaikh <sultan.shaikh.1585@gmail.com>
2022-05-01 13:24:51 +0530

    demo commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,18 @@
+import { SnotifyPosition } from 'ng-snotify';
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  //baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
+  baseUrl : `http://localhost:4200/`,
+
+  snotifyConfig : {
+    showProgressBar: true,
+    position: SnotifyPosition.rightTop,
+  },
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
